eb887aa2cbe5c27172d10230d19e7717621eb05e
Christopher Medlin <cjmedlin99@yahoo.com>
2018-02-28 16:28:11 -0800

    Add post to CavnasHandler

--- vimcanvas/handlers.py
+++ vimcanvas/handlers.py
@@ -3,9 +3,12 @@ from tornado.web import RequestHandler
 from tornado.options import options
 
 import pymongo
-from bson import json_util
+from bson import json_util, ObjectId
 
-class DatabaseMixin(object):
+import json
+import hashlib
+
+class HandlerMixin(object):
 
     @property
     def db(self):
@@ -13,12 +16,38 @@ class DatabaseMixin(object):
             self._mongo = pymongo.MongoClient(options.mongo_uri)
         return self._mongo.db
 
-class CanvasHandler(RequestHandler, DatabaseMixin):
+    def json_error(self, message):
+        self.write('{"error": "{}"}'.format(message))
+
+
+class CanvasHandler(RequestHandler, HandlerMixin):
     def get(self):
         if (self.get_cookie("user_id")):
-            query = {"$or": [{"owner": self.get_cookie("user_id")},
+            query = {"$or": [{"owner": ObjectId(self.get_cookie("user_id"))},
                              {"active": True}]}
         else:
             query = {"active": True}
 
         self.write(json_util.dumps(self.db.canvases.find(query)))
+
+    def post(self):
+        data = json.loads(self.request.body)
+        try:
+            data = {
+                "title": data["title"],
+                "owner": ObjectId(data["owner"]),
+                "active": True,
+                "alteredCharacters": [],
+            }
+        except KeyError:
+            self.json_error("One of the following fields are missing: title, owner")
+        
+        if len(data["title"]) > 50:
+            self.json_error("Title must be shorter than 50 characters")
+        self.db.canvases.insert_one(data)
+
+
+class UserCreateHandler(RequestHandler, HandlerMixin):
+    def post(self):
+        data = json.loads(self.request.body)
+        
