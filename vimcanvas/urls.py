7b8031d19bebb56d0b68c9751766a77937d1bd68
Christopher Medlin <cjmedlin99@yahoo.com>
2018-04-02 23:37:25 -0700

    Remove leading "/api" from urls.

--- vimcanvas/urls.py
+++ vimcanvas/urls.py
@@ -5,7 +5,7 @@ This file is for mapping URLs to handlers.
 from . import handlers, sockets
 
 url_config = [
-    (r"/api/v1/canvases", handlers.CanvasHandler),
-    (r"/api/v1/canvases/([^/]+)", handlers.CanvasRetrieveHandler),
-    (r"/api/v1/socket", sockets.CanvasWebSocketHandler)
+    (r"/v1/canvases", handlers.CanvasHandler),
+    (r"/v1/canvases/([^/]+)", handlers.CanvasRetrieveHandler),
+    (r"/v1/socket", sockets.CanvasWebSocketHandler)
 ]
